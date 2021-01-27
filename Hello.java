0b2b7fd0b69abc85b441e7df54b35c8eed9021e4
alan-leal <78099059+alan-leal@users.noreply.github.com>
2021-01-27 11:56:03 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+   public static void main(String[] args) {
+   	System.out.println("Alan Leal");
+   	System.out.println("HelloWorld");
+   	System.out.println("Lab1");
+   	System.out.println("January 27th 2021");
+   	System.out.println("CMSC 255 003");
+   	System.out.println();
+    }
+}    
